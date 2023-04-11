001889cf46ecfdcb0eb64f9b9e4694616081d54a
reddyvivek909@gmail.com <reddyvivek909@gmail.com>
2023-04-11 16:22:25 +0200

    key

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -3,4 +3,4 @@ mkdir keys
 echo $CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --client-id $APP_KEY --jwt-key-file keys/server.key --username $SF_USERNAME --set-default-dev-hub -a DevHub
+sfdx force:auth:jwt:grant --client-id $APP_KEY --jwt-key-file keys/jwtprivate.key --username $SF_USERNAME --set-default-dev-hub -a DevHub
