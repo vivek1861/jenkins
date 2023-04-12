48fcf9c2ac9a1fb19febe5e62a78cba3169d0db0
reddyvivek909@gmail.com <reddyvivek909@gmail.com>
2023-04-12 15:01:33 +0200

    config test

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -18,5 +18,5 @@
 echo "Deploying source to org"
 sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
 
-echo "Testing code in org"
-sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
+#echo "Testing code in org"
+#sfdx force:apex:test:run --testlevel RunLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
